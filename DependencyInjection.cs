7d7307c0b993797b56da829479ceca64e58eb639
Krishna Kumar <thisiskrsna11@gmail.com>
2026-05-04 12:26:12 +0530

    Add project files.

---
+++ DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MoneyGo.Application;
+using MoneyGo.Infrastructure;
+
+namespace MoneyGo.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI()
+                .AddInfrastructureDI();
+
+            return services;
+        }
+    }
+}
