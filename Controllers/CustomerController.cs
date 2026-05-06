7c62d4bd731f98dd3165c28a7cd40b082d2f8216
Krishna Kumar <thisiskrsna11@gmail.com>
2026-05-06 07:24:45 +0530

    Update: First endpoint added with mediatr

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MoneyGo.Application.Commands.CustomerCommands;
+using MoneyGo.Core.Entities;
+
+namespace MoneyGo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CustomerController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNewCustomer([FromBody] Customer customer)
+        {
+            var result = await _mediator.Send(new AddCustomerCommand(customer));
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCustomers()
+        {
+            return Ok();
+        }
+
+    }
+}
