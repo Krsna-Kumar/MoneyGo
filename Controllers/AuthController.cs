3a03bab347072b242ae52fbcbea0ecc9aaee8694
Krishna Kumar <thisiskrsna11@gmail.com>
2026-05-09 20:26:50 +0530

    Update: Added Auth Controller for Register and Login User Endpoints

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using MoneyGo.Application.Common.LoginUser;
+using MoneyGo.Application.Common.RegisterUser;
+
+namespace MoneyGo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public class AuthController
+        (IMediator mediator): ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] string username, string password)
+        {
+            var registeredUser = await mediator.Send(new RegisterUserCommand(username, password));
+            return registeredUser.IsSuccess
+                ? Ok(registeredUser.Value)
+                : BadRequest(registeredUser.Error);
+        }
+
+        [HttpGet("login")]
+        public async Task<IActionResult> LoginUser([FromBody] string username, string password)
+        {
+            var loggedInUser = await mediator.Send(new LoginUserCommnad(username, password));
+            return loggedInUser.IsSuccess
+                ? Ok(loggedInUser.Value)
+                : BadRequest(loggedInUser.Error);
+        }
+    }
+}
