bc5a2ef64d6600bdb8df16f77c4fe6ca8bf5ba65
Krishna Kumar <thisiskrsna11@gmail.com>
2026-05-09 08:12:20 +0530

    Update: Splitted Customer Controller into CustomerTransactionController

--- Controllers/CustomerTransactionsController.cs
+++ Controllers/CustomerTransactionsController.cs
@@ -1,6 +1,6 @@
 ﻿namespace MoneyGo.Api.Controllers
 {
-    [Route("api/customers/{customerId:int}/transactions")]
+    [Route("api/customers/{id:int}/transactions")]
     [ApiController]
     public class CustomerTransactionsController
         (IMediator mediator): ControllerBase
@@ -12,21 +12,21 @@
             return result.IsSuccess ? Ok(result.Value) : NotFound(result.Error);
         }
 
-        [HttpPost("/credit")]
-        public async Task<IActionResult> CreateCreditTransaction(int custId, [FromBody] TransactionRequest creditTrn)
+        [HttpPost("credit")]
+        public async Task<IActionResult> CreateCreditTransaction(int id, [FromBody] TransactionRequest creditTrn)
         {
-            var creditResult = await mediator.Send(new AddCreditTransactionCommand(custId, creditTrn));
+            var creditResult = await mediator.Send(new AddCreditTransactionCommand(id, creditTrn));
             return creditResult.IsSuccess ? Ok(creditResult.Value) : BadRequest(creditResult.Error);
         }
 
-        [HttpPost("/payment")]
-        public async Task<IActionResult> CreatePaymentTransaction(int custId, [FromBody] TransactionRequest paymentTrn)
+        [HttpPost("payment")]
+        public async Task<IActionResult> CreatePaymentTransaction(int id, [FromBody] TransactionRequest paymentTrn)
         {
-            var paymentResult = await mediator.Send(new AddPaymentTransactionCommand(custId, paymentTrn));
+            var paymentResult = await mediator.Send(new AddPaymentTransactionCommand(id, paymentTrn));
             return paymentResult.IsSuccess ? Ok(paymentResult.Value) : BadRequest(paymentResult.Error);
         }
 
-        [HttpGet("/balance")]
+        [HttpGet("balance")]
         public async Task<IActionResult> GetBalanceById(int id)
         {
             var result = await mediator.Send(new GetBalanceByIdQuery(id));
