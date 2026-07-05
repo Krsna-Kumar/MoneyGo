3f2dd5c8fe5150dbe8b00c30111b6714d60b52e4
Krishna Kumar <thisiskrsna11@gmail.com>
2026-07-05 18:01:20 +0530

    Update: Minor Tweak

--- Program.cs
+++ Program.cs
@@ -54,14 +54,14 @@ namespace MoneyGo.Api
 
                         options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
                     });
-                // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
                 builder.Services.AddOpenApi();
             }
             
 
             var app = builder.Build();
             {
-                // Configure the HTTP request pipeline.
+
                 if (app.Environment.IsDevelopment())
                 {
                     app.MapOpenApi();
